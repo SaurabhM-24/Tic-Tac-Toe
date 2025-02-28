82792931bc1aaf6bce10d0df1193f4dfcfa5161e
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-02-28 23:07:47 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -2,6 +2,7 @@
 from random import choice
 
 #function for getting the position with the help of inputs
+
 def position(row, col, symbl, D):
     if row.upper() == 'A':
         if col == '1' and D['TL'] == ' ':
