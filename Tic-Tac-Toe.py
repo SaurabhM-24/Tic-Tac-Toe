9c25db3dc9f1e7cd02d3dbb771cf88930d35ad91
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-04-13 23:56:07 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -11,7 +11,6 @@ def position(row, col, symbl, D):
             D['TM'] = symbl
         elif col == '3' and D['TR'] == ' ':
             D['TR'] = symbl
-            
         else:
             if symbl == P_symbl:
                 user = 'player'
