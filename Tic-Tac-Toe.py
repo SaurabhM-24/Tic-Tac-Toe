a123cd84ed3c772682d8beada917b586591fff84
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-05-22 22:42:27 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,8 +1,6 @@
 #importing choice function from random module for computer moves
 from random import choice
 
-
-
 #function for getting the position with the help of input
 def position(row, col, symbl, D):
     
