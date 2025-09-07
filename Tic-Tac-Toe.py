16a3ad3336c57612dad85d8034f636fd1952b77f
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-09-07 23:06:53 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -11,7 +11,6 @@ def position(row, col, symbl, D):
             D['TM'] = symbl
         elif col == '3' and D['TR'] == ' ':
             D['TR'] = symbl     
-            
         else:
             if symbl == P_symbl:
                 user = 'player'
@@ -174,6 +173,7 @@ while run == True:
         if ask.upper() == 'N':
             print('Thank you for playing this game!')
             run = False
+
 
 
 
